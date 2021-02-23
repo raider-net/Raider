29dd4a64725eb40d84cf9dab1a70675d3c045f60
Tomas Pilat <tomas.pilat@disig.sk>
2021-02-23 10:44:16 +0100

    Raider.Core: added Guid converter

---
+++ src/Raider.Core/Converters/GuidConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Raider.Converters
+{
+	public static class GuidConverter
+	{
+		public static Guid IntToGuid(int value)
+		{
+			byte[] bytes = new byte[16];
+			BitConverter.GetBytes(value).CopyTo(bytes, 0);
+			return new Guid(bytes);
+		}
+
+		public static int GuidToInt(Guid value)
+		{
+			byte[] b = value.ToByteArray();
+			int bint = BitConverter.ToInt32(b, 0);
+			return bint;
+		}
+	}
+}
