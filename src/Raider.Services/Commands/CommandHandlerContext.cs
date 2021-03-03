35fdb46d35a243febd3fee8a849e111c9e8dedb5
Tomas Pilat <tomas.pilat@disig.sk>
2021-03-03 10:03:22 +0100

    Raider.Services: updated CommandHandlerContext logger

--- src/Raider.Services/Commands/CommandHandlerContext.cs
+++ src/Raider.Services/Commands/CommandHandlerContext.cs
@@ -7,6 +7,7 @@ using Raider.EntityFrameworkCore;
 using Raider.Identity;
 using Raider.Localization;
 using Raider.Logging;
+using Raider.Logging.Extensions;
 using Raider.Trace;
 using System;
 using System.Collections.Concurrent;
@@ -146,14 +147,10 @@ namespace Raider.Services.Commands
 			if (message == null)
 				throw new ArgumentNullException(nameof(message));
 
-			if (!Logger.IsEnabled(LogLevel.Trace))
-				return;
-
-			message.LogLevel = LogLevel.Trace;
 			if (string.IsNullOrWhiteSpace(message.CommandQueryName))
 				message.CommandQueryName = CommandName;
 
-			Logger.LogTrace($"{LoggerSettings.FWK_LogMessage_Template}", message.ToDictionary());
+			Logger.LogTraceMessage(message);
 		}
 
 		public ILogMessage? LogTraceMessage(MethodLogScope scope, Action<LogMessageBuilder> messageBuilder)
@@ -164,19 +161,7 @@ namespace Raider.Services.Commands
 			if (messageBuilder == null)
 				throw new ArgumentNullException(nameof(messageBuilder));
 
-			if (!Logger.IsEnabled(LogLevel.Trace))
-				return null;
-
-			var builder = new LogMessageBuilder(scope.TraceInfo)
-				.LogLevel(LogLevel.Trace)
-				.CommandQueryName(CommandName);
-
-			messageBuilder.Invoke(builder);
-			var message = builder.Build();
-
-			Logger.LogTrace($"{LoggerSettings.FWK_LogMessage_Template}", message.ToDictionary());
-
-			return message;
+			return Logger.LogTraceMessage(scope, (x => x.CommandQueryName(CommandName)) + messageBuilder);
 		}
 
 		public void LogDebugMessage(ILogMessage message)
@@ -184,14 +169,10 @@ namespace Raider.Services.Commands
 			if (message == null)
 				throw new ArgumentNullException(nameof(message));
 
-			if (!Logger.IsEnabled(LogLevel.Debug))
-				return;
-
-			message.LogLevel = LogLevel.Debug;
 			if (string.IsNullOrWhiteSpace(message.CommandQueryName))
 				message.CommandQueryName = CommandName;
 
-			Logger.LogDebug($"{LoggerSettings.FWK_LogMessage_Template}", message.ToDictionary());
+			Logger.LogDebugMessage(message);
 		}
 
 		public ILogMessage? LogDebugMessage(MethodLogScope scope, Action<LogMessageBuilder> messageBuilder)
@@ -202,19 +183,7 @@ namespace Raider.Services.Commands
 			if (messageBuilder == null)
 				throw new ArgumentNullException(nameof(messageBuilder));
 
-			if (!Logger.IsEnabled(LogLevel.Debug))
-				return null;
-
-			var builder = new LogMessageBuilder(scope.TraceInfo)
-				.LogLevel(LogLevel.Debug)
-				.CommandQueryName(CommandName);
-
-			messageBuilder.Invoke(builder);
-			var message = builder.Build();
-
-			Logger.LogDebug($"{LoggerSettings.FWK_LogMessage_Template}", message.ToDictionary());
-
-			return message;
+			return Logger.LogDebugMessage(scope, (x => x.CommandQueryName(CommandName)) + messageBuilder);
 		}
 
 		public void LogInformationMessage(ILogMessage message)
@@ -222,14 +191,10 @@ namespace Raider.Services.Commands
 			if (message == null)
 				throw new ArgumentNullException(nameof(message));
 
-			if (!Logger.IsEnabled(LogLevel.Information))
-				return;
-
-			message.LogLevel = LogLevel.Information;
 			if (string.IsNullOrWhiteSpace(message.CommandQueryName))
 				message.CommandQueryName = CommandName;
 
-			Logger.LogInformation($"{LoggerSettings.FWK_LogMessage_Template}", message.ToDictionary());
+			Logger.LogInformationMessage(message);
 		}
 
 		public ILogMessage? LogInformationMessage(MethodLogScope scope, Action<LogMessageBuilder> messageBuilder)
@@ -240,19 +205,7 @@ namespace Raider.Services.Commands
 			if (messageBuilder == null)
 				throw new ArgumentNullException(nameof(messageBuilder));
 
-			if (!Logger.IsEnabled(LogLevel.Information))
-				return null;
-
-			var builder = new LogMessageBuilder(scope.TraceInfo)
-				.LogLevel(LogLevel.Information)
-				.CommandQueryName(CommandName);
-
-			messageBuilder.Invoke(builder);
-			var message = builder.Build();
-
-			Logger.LogInformation($"{LoggerSettings.FWK_LogMessage_Template}", message.ToDictionary());
-
-			return message;
+			return Logger.LogInformationMessage(scope, (x => x.CommandQueryName(CommandName)) + messageBuilder);
 		}
 
 		public void LogWarningMessage(ILogMessage message)
@@ -260,14 +213,10 @@ namespace Raider.Services.Commands
 			if (message == null)
 				throw new ArgumentNullException(nameof(message));
 
-			if (!Logger.IsEnabled(LogLevel.Warning))
-				return;
-
-			message.LogLevel = LogLevel.Warning;
 			if (string.IsNullOrWhiteSpace(message.CommandQueryName))
 				message.CommandQueryName = CommandName;
 
-			Logger.LogWarning($"{LoggerSettings.FWK_LogMessage_Template}", message.ToDictionary());
+			Logger.LogWarningMessage(message);
 		}
 
 		public ILogMessage? LogWarningMessage(MethodLogScope scope, Action<LogMessageBuilder> messageBuilder)
@@ -278,19 +227,7 @@ namespace Raider.Services.Commands
 			if (messageBuilder == null)
 				throw new ArgumentNullException(nameof(messageBuilder));
 
-			if (!Logger.IsEnabled(LogLevel.Warning))
-				return null;
-
-			var builder = new LogMessageBuilder(scope.TraceInfo)
-				.LogLevel(LogLevel.Warning)
-				.CommandQueryName(CommandName);
-
-			messageBuilder.Invoke(builder);
-			var message = builder.Build();
-
-			Logger.LogWarning($"{LoggerSettings.FWK_LogMessage_Template}", message.ToDictionary());
-
-			return message;
+			return Logger.LogWarningMessage(scope, (x => x.CommandQueryName(CommandName)) + messageBuilder);
 		}
 
 		public void LogErrorMessage(IErrorMessage message)
@@ -298,11 +235,10 @@ namespace Raider.Services.Commands
 			if (message == null)
 				throw new ArgumentNullException(nameof(message));
 
-			message.LogLevel = LogLevel.Error;
 			if (string.IsNullOrWhiteSpace(message.CommandQueryName))
 				message.CommandQueryName = CommandName;
 
-			Logger.LogError($"{LoggerSettings.FWK_LogMessage_Template}", message.ToDictionary());
+			Logger.LogErrorMessage(message);
 		}
 
 		public IErrorMessage LogErrorMessage(MethodLogScope scope, Action<ErrorMessageBuilder> messageBuilder)
@@ -313,16 +249,7 @@ namespace Raider.Services.Commands
 			if (messageBuilder == null)
 				throw new ArgumentNullException(nameof(messageBuilder));
 
-			var builder = new ErrorMessageBuilder(scope.TraceInfo)
-				.LogLevel(LogLevel.Error)
-				.CommandQueryName(CommandName);
-
-			messageBuilder.Invoke(builder);
-			var message = builder.Build();
-
-			Logger.LogError($"{LoggerSettings.FWK_LogMessage_Template}", message.ToDictionary());
-
-			return message;
+			return Logger.LogErrorMessage(scope, (x => x.CommandQueryName(CommandName)) + messageBuilder);
 		}
 
 		public void LogCriticalMessage(IErrorMessage message)
@@ -330,11 +257,10 @@ namespace Raider.Services.Commands
 			if (message == null)
 				throw new ArgumentNullException(nameof(message));
 
-			message.LogLevel = LogLevel.Critical;
 			if (string.IsNullOrWhiteSpace(message.CommandQueryName))
 				message.CommandQueryName = CommandName;
 
-			Logger.LogCritical($"{LoggerSettings.FWK_LogMessage_Template}", message.ToDictionary());
+			Logger.LogCriticalMessage(message);
 		}
 
 		public IErrorMessage LogCriticalMessage(MethodLogScope scope, Action<ErrorMessageBuilder> messageBuilder)
@@ -345,16 +271,7 @@ namespace Raider.Services.Commands
 			if (messageBuilder == null)
 				throw new ArgumentNullException(nameof(messageBuilder));
 
-			var builder = new ErrorMessageBuilder(scope.TraceInfo)
-				.LogLevel(LogLevel.Critical)
-				.CommandQueryName(CommandName);
-
-			messageBuilder.Invoke(builder);
-			var message = builder.Build();
-
-			Logger.LogCritical($"{LoggerSettings.FWK_LogMessage_Template}", message.ToDictionary());
-
-			return message;
+			return Logger.LogCriticalMessage(scope, (x => x.CommandQueryName(CommandName)) + messageBuilder);
 		}
 
 
