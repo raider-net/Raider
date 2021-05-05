aa1e1dc579100526b4db17f1ef5fa00a48c0f8a7
Tomas Pilat <tomas.pilat@disig.sk>
2021-05-05 17:38:24 +0200

    Raider.Localization: updated StringLocalizerExtensions

--- src/Raider.Localization/Extensions/Microsoft/Extensions/Localization/StringLocalizerExtensions.cs
+++ src/Raider.Localization/Extensions/Microsoft/Extensions/Localization/StringLocalizerExtensions.cs
@@ -6,7 +6,11 @@ namespace Raider.Extensions
 	public static class StringLocalizerExtensions
 	{
 		[return: NotNullIfNotNull("defaultText")]
-		public static string? GetLocalizedString(this IStringLocalizer localizer, string resourceKey, string? defaultText = null)
+		public static string? GetLocalizedString(this IStringLocalizer localizer, string resourceKey)
+			=> GetLocalizedString(localizer, resourceKey, resourceKey);
+
+		[return: NotNullIfNotNull("defaultText")]
+		public static string? GetLocalizedString(this IStringLocalizer localizer, string resourceKey, string? defaultText)
 		{
 			if (localizer == null)
 			{
@@ -27,7 +31,7 @@ namespace Raider.Extensions
 		}
 
 		[return: NotNullIfNotNull("defaultText")]
-		public static string? GetLocalizedString(this IStringLocalizer localizer, string resourceKey, string? defaultText = null, params object[] arguments)
+		public static string? GetLocalizedString(this IStringLocalizer localizer, string resourceKey, string? defaultText, params object[] arguments)
 		{
 			if (localizer == null)
 			{
@@ -43,6 +47,64 @@ namespace Raider.Extensions
 				else
 				{
 					return localizedString;
+				}
+			}
+		}
+
+		public static bool TryGetLocalizedString(this IStringLocalizer localizer, string resourceKey, [NotNullWhen(true)] out string? localizedValue)
+		{
+			localizedValue = null;
+			if (localizer == null)
+			{
+				return false;
+			}
+			else
+			{
+				var localizedString = localizer[resourceKey];
+				if (localizedString.ResourceNotFound)
+				{
+					return false;
+				}
+				else
+				{
+					if (localizedString == null)
+					{
+						return false;
+					}
+					else
+					{
+						localizedValue = localizedString;
+						return true;
+					}
+				}
+			}
+		}
+
+		public static bool TryGetLocalizedString(this IStringLocalizer localizer, string resourceKey, [NotNullWhen(true)] out string? localizedValue, params object[] arguments)
+		{
+			localizedValue = null;
+			if (localizer == null)
+			{
+				return false;
+			}
+			else
+			{
+				var localizedString = localizer[resourceKey, arguments];
+				if (localizedString.ResourceNotFound)
+				{
+					return false;
+				}
+				else
+				{
+					if (localizedString == null)
+					{
+						return false;
+					}
+					else
+					{
+						localizedValue = localizedString;
+						return true;
+					}
 				}
 			}
 		}
