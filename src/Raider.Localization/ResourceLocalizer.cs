eb620bf03313d529d5e5484850e03ab76b09abbc
Tomas Pilat <tomas.pilat@disig.sk>
2021-05-08 10:15:29 +0200

    Raider.Localization: added ResourceLocalizerFactory and ResourceLocalizer

---
+++ src/Raider.Localization/ResourceLocalizer.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Localization;
+using System;
+
+namespace Raider.Localization
+{
+	public interface IResourceLocalizer
+	{
+		IStringLocalizer Localizer { get; }
+	}
+
+	public interface IResourceLocalizer<TKeys> : IResourceLocalizer
+	{
+	}
+
+	public abstract class ResourceLocalizer<TKeys> : IResourceLocalizer<TKeys>
+	{
+		public IStringLocalizer Localizer { get; }
+
+		public ResourceLocalizer(IServiceProvider serviceProvider, string baseName, string assemblyName)
+		{
+			Localizer = ResourceLocalizerFactory.Create(serviceProvider, baseName, assemblyName);
+		}
+
+		public ResourceLocalizer(IStringLocalizer localizer)
+		{
+			Localizer = localizer ?? throw new ArgumentNullException(nameof(localizer));
+		}
+	}
+}
