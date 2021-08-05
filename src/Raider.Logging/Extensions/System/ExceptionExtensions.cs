aaff24092ffe446e5146907f7f2053a68a46b202
Tomas Pilat <tomas.pilat@disig.sk>
2021-08-05 14:10:54 +0200

    Raider.Logging added AppendLogMessage to ExceptionExtensions

--- src/Raider.Logging/Extensions/System/ExceptionExtensions.cs
+++ src/Raider.Logging/Extensions/System/ExceptionExtensions.cs
@@ -7,6 +7,30 @@ namespace Raider.Extensions
 	{
 		private const string RAIDER_LOG_MESSAGE = nameof(RAIDER_LOG_MESSAGE);
 
+		public static T AppendLogMessage<T>(this T exception, ILogMessage logMessage)
+			where T : Exception
+		{
+			if (exception == null)
+				throw new ArgumentNullException(nameof(exception));
+
+			if (exception.Data.Contains(RAIDER_LOG_MESSAGE))
+			{
+				var value = exception.Data[RAIDER_LOG_MESSAGE];
+				if (value is ErrorMessage msg)
+				{
+					msg.Detail = string.IsNullOrWhiteSpace(msg.Detail)
+						? $"---NEXT LOG MESSAGE---{Environment.NewLine}{logMessage.FullMessage}"
+						: $"{msg.Detail}{Environment.NewLine}---NEXT LOG MESSAGE---{Environment.NewLine}{logMessage.FullMessage}";
+				}
+			}
+			else
+			{
+				exception.Data[RAIDER_LOG_MESSAGE] = logMessage;
+			}
+
+			return exception;
+		}
+
 		public static T SetLogMessage<T>(this T exception, ILogMessage logMessage)
 			where T : Exception
 		{
